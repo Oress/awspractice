a32fad50e0e813d7310bfb2f727ceb637eb8b9d4
Ivan Paniotov <ivanpaniotov@gmail.com>
2024-05-24 16:14:21 +0000

    [s3][00] Final

--- sqs/00/producer/aws/userdata.sh
+++ sqs/00/producer/aws/userdata.sh
@@ -5,7 +5,7 @@ sudo yum update -y \
     && sudo yum install java-21-amazon-corretto -y
 
 # Download JAR package from S3
-aws s3 cp s3://queue-src00/consumer/app.jar /root/app.jar
+aws s3 cp s3://queue-src00/producer/app.jar /root/app.jar
 
 # Start the app
 java -Dsqs.queue.name=basic-q -Dsqs.region=us-east-1 -jar /root/app.jar
--- sqs/00/script.sh
+++ sqs/00/script.sh
@@ -28,8 +28,31 @@ aws sqs get-queue-attributes --queue-url https://sqs.us-east-1.amazonaws.com/450
 aws iam create-role --role-name sqs00consumer --assume-role-policy-document file://assume-role-policy.json
 aws iam create-policy --policy-name sqs00consumer-policy --policy-document file://consumer/aws/ec2rolepolicy.json
 aws iam attach-role-policy --role-name sqs00consumer --policy-arn arn:aws:iam::450346197415:policy/sqs00consumer-policy
+aws iam create-instance-profile --instance-profile-name sqs00consumer-ipn
+aws iam add-role-to-instance-profile --instance-profile-name sqs00consumer-ipn --role-name sqs00consumer
+
 
 aws iam create-role --role-name sqs00producer --assume-role-policy-document file://assume-role-policy.json
+aws iam create-policy --policy-name sqs00producer-policy --policy-document file://producer/aws/ec2rolepolicy.json
+aws iam attach-role-policy --role-name sqs00producer --policy-arn arn:aws:iam::450346197415:policy/sqs00producer-policy
+aws iam create-instance-profile --instance-profile-name sqs00producer-ipn
+aws iam add-role-to-instance-profile --instance-profile-name sqs00producer-ipn --role-name sqs00producer
+
+aws ec2 run-instances --image-id ami-07caf09b362be10b8\
+    --iam-instance-profile Name=sqs00consumer-ipn \
+    --instance-type t2.micro \
+    --key-name test \
+    --user-data file://consumer/aws/userdata.sh \
+    --security-group-ids sg-0f49bf7f2faf92eb8 sg-020d0b6bf5b898fd9 sg-074c4771bb2362c9c
+
+
+aws ec2 run-instances --image-id ami-07caf09b362be10b8\
+    --iam-instance-profile Name=sqs00producer-ipn \
+    --instance-type t2.micro \
+    --key-name test \
+    --user-data file://producer/aws/userdata.sh \
+    --security-group-ids sg-0f49bf7f2faf92eb8 sg-020d0b6bf5b898fd9 sg-074c4771bb2362c9c
+
 
 
 
