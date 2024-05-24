5d0f0bc12c58fe0216b50249580ccd8b9ac8cd3e
Ivan Paniotov <ivanpaniotov@gmail.com>
2024-05-24 19:14:30 +0000

    [cloudwatch][00] Init

---
+++ cloudwatch/00/custom-metrics/src/main/java/org/ipan/cloudwatch/custom_metrics/CustomMetricsApplication.java
@@ -0,0 +1,18 @@
+package org.ipan.cloudwatch.custom_metrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class CustomMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomMetricsApplication.class, args);
+	}
+
+
+
+	
+}
